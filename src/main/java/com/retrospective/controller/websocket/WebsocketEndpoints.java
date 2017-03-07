bd1886889b08f47b0cd0a7580b8d06e7eacef446
akos.sereg <akos.sereg@gmail.com>
2017-03-07 21:14:34 +0100

    Removing participant list

--- src/main/java/com/retrospective/controller/websocket/WebsocketEndpoints.java
+++ src/main/java/com/retrospective/controller/websocket/WebsocketEndpoints.java
@@ -1,7 +1,5 @@
 package com.retrospective.controller.websocket;
 
-import java.util.Hashtable;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -9,8 +7,6 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import com.retrospective.model.SessionDetails;
-import com.retrospective.model.websocket.Participant;
 import com.retrospective.model.websocket.ParticipantJoinedBroadcastMessage;
 import com.retrospective.model.websocket.ParticipantJoinedMessage;
 import com.retrospective.model.websocket.PublishStickerMessage;
@@ -18,16 +14,10 @@ import com.retrospective.model.websocket.PublishStickerMessage;
 @Controller
 public class WebsocketEndpoints {
 
-	/**
-	 * TODO: purge all participants periodically where lastSeen is long enough
-	 */
-	private Hashtable<String, Participant> participants;
-	
 	@Autowired 
 	private SimpMessagingTemplate simpMessagingTemplate;
 	
 	public WebsocketEndpoints() {
-		participants = new Hashtable<String, Participant>();
 	}
 	
 	@MessageMapping("/board/join/{code}/{token}")
@@ -35,35 +25,16 @@ public class WebsocketEndpoints {
 		
 		ParticipantJoinedBroadcastMessage result = new ParticipantJoinedBroadcastMessage();
 		result.setUsername(message.getUsername());
-
-		synchronized (this.participants) {
-			String participantKey = this.getParticipantKey(code, token, message.getId());
-			if (participants.containsKey(participantKey)) {
-				long lastSeen = System.currentTimeMillis() / 1000L;
-				result.setKeepalive(true);
-				result.setLastSeen(lastSeen);
-				this.participants.get(participantKey).setLastSeen(lastSeen);
-				simpMessagingTemplate.convertAndSend("/topic/join/" + code + "/" + token, result);
-				return;
-			}
-
-			Participant participant = new Participant();
-			participant.setParticipantDetails(message);
-			participant.setSession(new SessionDetails(code, token));
-			this.participants.put(participantKey, participant);
-
-			result.setConnect(true);
-			simpMessagingTemplate.convertAndSend("/topic/join/" + code + "/" + token, result);
-			return;
-		}
+		
+		simpMessagingTemplate.convertAndSend(
+				String.format("/topic/join/%s/%s", code, token), 
+				result);
     }
 	
 	@MessageMapping("/board/sticker/{code}/{token}")
     public void websocketPublishSticker(@Payload PublishStickerMessage message, @DestinationVariable("code") int code, @DestinationVariable("token") String token) throws Exception {
-		simpMessagingTemplate.convertAndSend("/topic/sticker/" + code + "/" + token, message);
+		simpMessagingTemplate.convertAndSend(
+				String.format("/topic/sticker/%s/%s", code, token), 
+				message);
     }
-	
-	private String getParticipantKey(int code, String token, String userId) {
-		return String.format("%d_%s_%s", code, token, userId);
-	}
 }
--- src/main/java/com/retrospective/model/websocket/ParticipantJoinedBroadcastMessage.java
+++ src/main/java/com/retrospective/model/websocket/ParticipantJoinedBroadcastMessage.java
@@ -2,31 +2,7 @@ package com.retrospective.model.websocket;
 
 public class ParticipantJoinedBroadcastMessage {
 
-	private boolean isKeepalive;
-	
-	private boolean isConnect;
-	
 	private String username;
-
-	private long lastSeen;
-	
-	private long serverTime;
-	
-	public boolean isKeepalive() {
-		return isKeepalive;
-	}
-
-	public void setKeepalive(boolean isKeepalive) {
-		this.isKeepalive = isKeepalive;
-	}
-
-	public boolean isConnect() {
-		return isConnect;
-	}
-
-	public void setConnect(boolean isConnect) {
-		this.isConnect = isConnect;
-	}
 
 	public String getUsername() {
 		return username;
@@ -35,22 +11,4 @@ public class ParticipantJoinedBroadcastMessage {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-	public long getLastSeen() {
-		return lastSeen;
-	}
-
-	public void setLastSeen(long lastSeen) {
-		this.lastSeen = lastSeen;
-	}
-
-	public long getServerTime() {
-		return serverTime;
-	}
-
-	public void setServerTime(long serverTime) {
-		this.serverTime = serverTime;
-	}
-	
-	
 }
